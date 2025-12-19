ea071678fbc41b8fabb8f34d4dabe3da94f44e45
Mukeshkumar <mukeshkumar87086@gmail.com>
2025-12-19 13:28:05 +0530

    selva

--- client/src/Uniform/Components/MaterialIssue/MaterialIssueFormReport.js
+++ client/src/Uniform/Components/MaterialIssue/MaterialIssueFormReport.js
@@ -200,12 +200,12 @@ const MaterialIssueFormReport = ({
     return (
         <div
             //   id="registrationFormReport"
-            className="flex flex-col w-full h-[93%] overflow-auto"
+            className="flex flex-col w-full h-[78vh] overflow-auto"
         >
 
             <>
-                <div className="h-[100%] rounded-lg bg-[#F1F1F0] shadow-sm">
-                    <div className="h-[90%]">
+                <div className="h-[100vh] rounded-lg bg-[#F1F1F0] shadow-sm">
+                    <div className="h-[68vh]">
                         <table className="">
                             <thead className="bg-gray-200 text-gray-800 ">
                                 <tr className="">
@@ -372,7 +372,7 @@ const MaterialIssueFormReport = ({
                         </table>
 
                     </div>
-                    <div className="h-[10%]">
+                    <div className="h-[10vh]">
                         <Pagination />
                     </div>
 
