a05ee48b09fbf6d5e37823dd1ea1859540e2f690
karthick <karthickcs10124@gmail.com>
2026-09-08 13:39:29 +0530

    server limit

--- server.js
+++ server.js
@@ -84,8 +84,8 @@ import { socketMain } from './src/sockets/socket.js';
 import { getStockQty } from './src/controllers/stock.controller.js';
 
 const app = express()
-// app.use(express.json())
-app.use(express.urlencoded({ limit: "50mb", extended: true }))
+app.use(express.json({ limit: "50mb" }))
+// app.use(express.urlencoded({ limit: "50mb", extended: true }))
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
