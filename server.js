2ff95f5c082243733c00a34e26aae95ccf19f8a5
karthick <karthickcs10124@gmail.com>
2026-09-08 13:31:59 +0530

    server limit increase

--- server.js
+++ server.js
@@ -85,8 +85,7 @@ import { getStockQty } from './src/controllers/stock.controller.js';
 
 const app = express()
 // app.use(express.json())
-app.use(express.json({ limit: "50mb" }))
-
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -105,7 +104,6 @@ app.use(cors())
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.json())
 
 const path = __dirname + '/client/build/';
 
